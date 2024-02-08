c5a7f07c0ced7866c1d14bb0414402f14c56ddc7
Aryan Pandit <a12022005@gmail.com>
2024-02-09 01:02:33 +0530

    logoimage

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -2,6 +2,7 @@
     import Header from "./Header.svelte";
     import { writable } from "svelte/store";
     import { supabase } from "$lib/supabase";
+    
     const state = writable("dumb");
     const mode = writable("pfft");
 
@@ -15,12 +16,16 @@
     
     <div class=" text-6xl  font-Stint w-1/3 translate-y-28 translate-x-28 select-none">Are you bored today?</div>
     <div class=" h-[74svh] flex justify-center items-center">
+        <div class="flex justify-center">
+            <img src="%sveltekit.assets%/YDO.png" alt="You Deserve One!" class="h-10 w-10">
+          </div>
     <div class=" w-1/3 rounded-3xl h-14 flex-col flex justify-center items-center ">
         <div  class="flex justify-center items-center ">
             <button on:click={SigninWithGoogle} class="btn btn-outline btn-primary ext-base ">Login to find you match!</button>
         </div>
     </div>
     </div>
+    
 </div>
 
 
--- src/routes/Loginnavbar.svelte
+++ src/routes/Loginnavbar.svelte
@@ -1,32 +1,5 @@
-<main>
-    <div class="navbar bg-base-100">
-        <div class="navbar-start">
-            <div class="dropdown dropdown-hover">
-              <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-              </div>
-              <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-              <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52">
-                <li class="select-none"><a href="http://localhost:5173/">Need Help?</a></li>
-              </ul>
-            </div>
-          </div>
-        <div class="navbar-center">
-          <a href="http://localhost:5173/" class="text-xl font-serif  select-none">You Deserve One!</a>
-        </div>
-        <div class="navbar-end">
-          <button class="btn btn-ghost btn-circle">
-            <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-              <span class="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
-        <div class="avatar">
-            <div class="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 m-2">
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
-        </div>
-        </div>
-      </div>
-</main>
+<div class="flex justify-center">
+  <div class="flex justify-center">
+    <img src="ydo.png" alt="You Deserve One!" class="h-10 w-10">
+  </div>
+</div>
